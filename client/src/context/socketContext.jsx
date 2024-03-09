aa6355342b92bb12f5147b38c3412a76a67dc0f1
Shahnoor_Mujawar <144203745+shahnoorgit@users.noreply.github.com>
2024-03-09 13:55:08 +0530

    Update socketContext.jsx

--- client/src/context/socketContext.jsx
+++ client/src/context/socketContext.jsx
@@ -13,7 +13,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://devchats.onrender.com", {
         query: {
           userId: authUser._id,
         },
