e452ddb475bad9b9aac05069cb97ad455865cfe6
Shahnoor_Mujawar <144203745+shahnoorgit@users.noreply.github.com>
2024-04-18 13:55:51 +0530

    Update socket.js

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://devchats.onrender.com/",
     methods: ["GET", "POST"],
   },
 });
